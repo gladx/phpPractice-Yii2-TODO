27b07aeebd8ee06748e8594510d2e5ac59682553
iglad <aliyousefi.tec@gmail.com>
2017-09-21 12:48:16 +0430

    add language Farsi

--- config/console.php
+++ config/console.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic-console',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'fa-IR',    
     'controllerNamespace' => 'app\commands',
     'components' => [
         'cache' => [
--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'defaultRoute' => 'todo',
+    'language' => 'fa',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
